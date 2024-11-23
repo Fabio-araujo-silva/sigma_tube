e31e5ffd9bd808b56aa4d7059f35cb9337673b87
fabio-araujo-silva <fabio.kaue.araujo@gmail.com>
2024-11-23 11:39:56 -0300

    commit

--- sigma_tube.c
+++ sigma_tube.c
@@ -1,2 +1,27 @@
 #include <sigma_tube.h>
 
+int n_usp;
+int insereArvore();
+AbbAluno Arvore;
+
+int cadastraAluno(char *nome) {
+    Aluno aluno;
+    strcpy(aluno.nome, nome);
+    if ()
+    aluno.n_usp = n_usp++;
+}
+
+int listaAlunos(); // Lista todos os alunos cadastrados no sistema
+Aluno buscaAluno(); // Busca um aluno no sistema a partir do seu nome
+int listaFilme(); // Lista todos os filmes cadastrados em ordem alfabética
+int buscaFilme(); // Busca filme a partir de seu nome e verifica se foi assistido, retornando inteiro correspondente
+Aluno preferenciaConvergente(); // Aponta o aluno cujas preferências são mais compatíveis
+Aluno preferenciaDivergente(); // Aponta o aluno cujas preferências são menos compatíveis
+int geraRelatorio(); // Gera arquivo de texto com todas as informações do sistema
+int infosArvore(); // Exibe informações técnicas da ABB de alunos
+int retirarAluno(); // Apaga cadastro de um aluno
+
+// Funcionalidades adicionais
+int top10doMomento(); // Exibe os 10 filmes mais populares do momento
+int adicionaFilmeFavorito();
+int imprimeFilmesFavorito();
--- sigma_tube.h
+++ sigma_tube.h
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 #define MAX_NOME 40
 #define MAX_TITULO 40
