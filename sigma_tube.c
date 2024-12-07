5552f030b9835d235d9d581bffe0565055f38df7
Fabio Araujo <82675020+Fabio-araujo-silva@users.noreply.github.com>
2024-12-07 09:15:01 -0300

    Update sigma_tube.c

--- sigma_tube.c
+++ sigma_tube.c
@@ -15,6 +15,24 @@ int listaAlunos(); // Lista todos os alunos cadastrados no sistema
 Aluno buscaAluno(); // Busca um aluno no sistema a partir do seu nome
 int listaFilme(); // Lista todos os filmes cadastrados em ordem alfabética
 int buscaFilme(); // Busca filme a partir de seu nome e verifica se foi assistido, retornando inteiro correspondente
+Diferenca(Aluno aluno1, Aluno aluno2) {
+    double normaAluno1 = 0, normaAluno2 = 0, somaDiferencas = 0;
+
+    for (int i = 0; i < 6; i++) {
+        normaAluno1 += pow(aluno1->categorias[i], 2);
+        normaAluno2 += pow(aluno2->categorias[i], 2);
+    }
+
+    normaAluno1 = sqrt(normaAluno1);
+    normaAluno2 = sqrt(normaAluno2);
+
+    for (int i = 0; i < 6; i++) {
+        somaDiferencas += pow(aluno1->categorias[i] * normaAluno2 - aluno2->categorias[i] * normaAluno1, 2);
+    }
+
+    return (1 / (2 * normaAluno1 * normaAluno2 * somaDiferencas));
+}
+
 Aluno preferenciaConvergente(); // Aponta o aluno cujas preferências são mais compatíveis
 Aluno preferenciaDivergente(); // Aponta o aluno cujas preferências são menos compatíveis
 int geraRelatorio(); // Gera arquivo de texto com todas as informações do sistema
