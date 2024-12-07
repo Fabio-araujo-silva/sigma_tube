547dd3aba5a6836201dfe4163f061034da33a521
Fabio Araujo <82675020+Fabio-araujo-silva@users.noreply.github.com>
2024-12-07 09:21:11 -0300

    Update sigma_tube.h

--- sigma_tube.h
+++ sigma_tube.h
@@ -40,7 +40,7 @@ int listaAlunos(); // Lista todos os alunos cadastrados no sistema
 Aluno buscaAluno(); // Busca um aluno no sistema a partir do seu nome
 int listaFilme(); // Lista todos os filmes cadastrados em ordem alfabética
 int buscaFilme(); // Busca filme a partir de seu nome e verifica se foi assistido, retornando inteiro correspondente
-Diferenca(Aluno aluno1, Aluno aluno2);
+float Diferenca(Aluno aluno1, Aluno aluno2);
 Aluno preferenciaConvergente(); // Aponta o aluno cujas preferências são mais compatíveis
 Aluno preferenciaDivergente(); // Aponta o aluno cujas preferências são menos compatíveis
 int geraRelatorio(); // Gera arquivo de texto com todas as informações do sistema
